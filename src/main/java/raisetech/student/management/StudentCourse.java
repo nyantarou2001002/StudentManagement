7cc9bc867a18bc3e9976a6b014b700a773c6b555
Kotaro Ito <nyarukotaro_65005@icloud.com>
2024-08-24 20:59:49 +0900

    23課題

---
+++ src/main/java/raisetech/student/management/StudentCourse.java
@@ -0,0 +1,19 @@
+package raisetech.student.management;
+
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StudentCourse {
+
+  private String id;
+  private String studentId;
+  private String courseName;
+  private LocalDateTime courseStartAt;
+  private LocalDateTime courseEndAt;
+
+
+}
--- src/main/java/raisetech/student/management/StudentRepository.java
+++ src/main/java/raisetech/student/management/StudentRepository.java
@@ -1,27 +1,20 @@
 package raisetech.student.management;
 
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
-import org.springframework.web.bind.annotation.DeleteMapping;
 
 @Mapper
 public interface StudentRepository {
 
-  @Select("SELECT * FROM student WHERE name = #{name}")
-  Student searchByName(String name);
+  @Select("SELECT * FROM students")
+  List<Student> search();
 
-  @Insert("INSERT student values(#{name}, #{age})")
-  void registerStudent(String name, int age);
+  @Select("SELECT * FROM students_courses")
+  List<StudentCourse> searchCourse();
 
-  @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
-  void updateStudent(String name, int age);
 
-  @Delete("DELETE FROM student WHERE name = #{name}")
-  void deleteStudent(String name);
 
 
 }
