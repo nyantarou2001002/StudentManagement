589d47abf73e5246af930fe434a11f1702873482
Kotaro Ito <nyarukotaro_65005@icloud.com>
2024-08-27 08:18:20 +0900

    課題２８の前保存

--- src/main/java/raisetech/student/management/data/StudentCourses.java
+++ src/main/java/raisetech/student/management/data/StudentCourses.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class StudentCourse {
+public class StudentCourses {
 
   private String id;
   private String studentId;
--- src/main/java/raisetech/student/management/service/StudentService.java
+++ src/main/java/raisetech/student/management/service/StudentService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import raisetech.student.management.data.Student;
-import raisetech.student.management.data.StudentCourse;
+import raisetech.student.management.data.StudentCourses;
 import raisetech.student.management.repository.StudentRepository;
 
 @Service
@@ -19,27 +19,15 @@ public class StudentService {
   }
 
   public List<Student> searchStudentList(){
-    //検索処理
-    List<Student> studentList = repository.search();
 
-    //絞り込みをする。年齢が３０代の人のみを抽出する
-    List<Student> studentListOf30s = studentList.stream()
-        .filter(student -> student.getAge() >= 30 && student.getAge() < 40)
-        .toList();
 
-    return studentListOf30s;
+    return repository.search();
   }
 
-  public List<StudentCourse> searchStudentCourseList(){
-    //検索処理
-    List<StudentCourse> studentCourseList = repository.searchCourse();
+  public List<StudentCourses> searchStudentCourseList(){
 
-    //絞り込みで「Javaコース」のみ抽出する
-    List<StudentCourse> studentCourseListOfJava = studentCourseList.stream()
-        .filter(studentCourse -> studentCourse.getCourseName().equals("Javaコース"))
-        .toList();
 
-    return studentCourseListOfJava;
+    return repository.searchCourse();
   }
 
 }
