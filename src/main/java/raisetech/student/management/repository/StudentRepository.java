2e670ba9a21d7302b57a4cba6aa8b45f093a3daf
Kotaro Ito <nyarukotaro_65005@icloud.com>
2024-09-02 05:21:57 +0900

    Javaコース35　終了

--- src/main/java/raisetech/student/management/repository/StudentRepository.java
+++ src/main/java/raisetech/student/management/repository/StudentRepository.java
@@ -22,7 +22,7 @@ public interface StudentRepository {
    *
    * @return　受講生一覧(全件)
    */
-  @Select("SELECT * FROM students")
+
   List<Student> search();
 
   /**
@@ -31,7 +31,6 @@ public interface StudentRepository {
    * @param id　受講生ID
    * @return　受講生
    */
-  @Select("SELECT * FROM students WHERE id = #{id}")
   Student searchStudent(String id);
 
   /**
@@ -39,7 +38,7 @@ public interface StudentRepository {
    *
    * @return　受講生コース情報(全件)
    */
-  @Select("SELECT * FROM students_courses")
+  //@Select("SELECT * FROM students_courses")
   List<StudentCourse> searchStudentCourseList();
 
   /**
@@ -48,7 +47,7 @@ public interface StudentRepository {
    * @param studentId 受講生ID
    * @return 受講生IDに紐づく受講生コース情報
    */
-  @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
+  //@Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
   List<StudentCourse> searchStudentCourse(String id);
 
   /**
@@ -57,10 +56,10 @@ public interface StudentRepository {
    *
    * @param student　受講生
    */
-  @Insert(
-      "INSERT INTO students(name, kana_name, nickname, email, area, age, sex, remark, isDeleted)"
-          + "VALUES(#{name}, #{kanaName}, #{nickname}, #{email}, #{area}, #{age}, #{sex}, #{remark}, false)")
-  @Options(useGeneratedKeys = true, keyProperty = "id")
+  //@Insert(
+      //"INSERT INTO students(name, kana_name, nickname, email, area, age, sex, remark, isDeleted)"
+          //+ "VALUES(#{name}, #{kanaName}, #{nickname}, #{email}, #{area}, #{age}, #{sex}, #{remark}, false)")
+  //@Options(useGeneratedKeys = true, keyProperty = "id")
   void registerStudent(Student student);
 
 
@@ -69,10 +68,10 @@ public interface StudentRepository {
    *
    * @param studentCourse　受講生コース情報
    */
-  @Insert(
-      "INSERT INTO students_courses(student_id,course_name, course_start_at, course_end_at)"
-          + "VALUES(#{studentId}, #{courseName}, #{courseStartAt}, #{courseEndAt})")
-  @Options(useGeneratedKeys = true, keyProperty = "id")
+  //@Insert(
+      //"INSERT INTO students_courses(student_id,course_name, course_start_at, course_end_at)"
+          //+ "VALUES(#{studentId}, #{courseName}, #{courseStartAt}, #{courseEndAt})")
+  //@Options(useGeneratedKeys = true, keyProperty = "id")
   void registerStudentCourse(StudentCourse studentCourse);
 
 
@@ -81,9 +80,9 @@ public interface StudentRepository {
    *
    * @param student　受講生
    */
-  @Update(
-      "UPDATE students SET name = #{name}, kana_name = #{kanaName}, nickname = #{nickname},"
-          + " email = #{email}, area = #{area}, age = #{age}, sex = #{sex}, remark = #{remark}, isDeleted = #{isDeleted} WHERE id = #{id}")
+  //@Update(
+      //"UPDATE students SET name = #{name}, kana_name = #{kanaName}, nickname = #{nickname},"
+          //+ " email = #{email}, area = #{area}, age = #{age}, sex = #{sex}, remark = #{remark}, isDeleted = #{isDeleted} WHERE id = #{id}")
   void updaterStudent(Student student);
 
   /**
@@ -92,8 +91,8 @@ public interface StudentRepository {
    *
    * @param studentCourse　受講生コース情報
    */
-  @Update(
-      "UPDATE students_courses SET course_name = #{courseName} WHERE student_id = #{studentId}")
+  //@Update(
+      //"UPDATE students_courses SET course_name = #{courseName} WHERE student_id = #{studentId}")
   void updateStudentCourse(StudentCourse studentCourse);
 
 
